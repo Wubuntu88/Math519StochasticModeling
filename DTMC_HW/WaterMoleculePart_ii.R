28488803ebca3570871b0e989d4e25d04628bb87
William Gillespie <wgilles1@emich.edu>
2017-03-26 18:46:13 -0400

    Tried to ignore backup Word files.

---
+++ DTMC_HW/WaterMoleculePart_ii.R
@@ -0,0 +1,18 @@
+temp = read.csv('WaterMoleculeTransitionMatrix.csv', header = FALSE)
+trans_matrix = as.matrix(temp)
+
+state_vector = t(as.matrix(c(0.2, 0.2, 0.2, 0.2, 0.2, 0.2)))
+header_values = c('Michigan', 'Superior', 'Huron', 'Eire', 'Ontario', 'Air')
+colnames(state_vector) = header_values
+print('initial state vector:')
+print(state_vector)
+
+iterations_left = 1000
+while(iterations_left > 0){
+  previous_state_vector = state_vector
+  state_vector = state_vector %*% trans_matrix
+  iterations_left = iterations_left - 1
+}
+print('steady state vector')
+print(header_values)
+print(sprintf('%.3f', state_vector))
