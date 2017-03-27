1b5d92182936a11f03eb68e0eed0dcaccc934141
William Gillespie <wgilles1@emich.edu>
2017-03-26 22:24:21 -0400

    Made it so that the state vector does its iteration, but I might have to change the transition matrix because we end up with 1/5 in each quintile (lol).

---
+++ DTMC_HW/ex2c_EconomicQuintiles.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import numpy as np
+
+transition_matrix = np.array(
+    [0.42, 0.23, 0.19, 0.11, 0.06,  # first quintile to other quintiles transition probabilities
+     0.25, 0.23, 0.24, 0.18, 0.10,  # second quintile...
+     0.17, 0.24, 0.23, 0.17, 0.19,  # third quintile...
+     0.08, 0.15, 0.19, 0.32, 0.26,  # fourth quintile...
+     0.09, 0.15, 0.14, 0.23, 0.39]  # fifth quintile...
+).reshape((5, 5))
+
+print('Transition matrix:')
+print(transition_matrix)
+
+p = [sum(x) for x in transition_matrix]
+print(p)
+
+# c) What is the steady-state distribution? Compute it using methods learned in class.
+# Answer, I will treat the 'quintiles' as income bins, that way it is possible for transitions between states
+state_vector = np.array([1, 1, 1, 1, 1])
+ITERATIONS_LIMIT = 1000
+i = 0
+while i < ITERATIONS_LIMIT:
+    state_vector = state_vector.dot(transition_matrix)
+    state_vector /= sum(state_vector)
+    i += 1
+print(state_vector)
